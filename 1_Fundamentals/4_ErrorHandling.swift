01df621eb7306ba56e6eec613c2fe55772a65eb7
Kevin Christian <kdotchrist30@gmail.com>
2024-03-04 09:49:00 +0700

    feat: added error handling

--- 1_Fundamentals/4_ErrorHandling.swift
+++ 1_Fundamentals/4_ErrorHandling.swift
@@ -21,3 +21,36 @@ do {
 } catch HeightError.minHeight {
   print("Min height error")
 }
+
+enum NameError : Error {
+  case noName
+}
+
+class Course {
+  var name: String
+
+  init(name: String) throws {
+    if name == "" {
+      throw NameError.noName
+    } else {
+      self.name = name
+      print("You've created an object!")
+    }
+  }
+}
+
+// The difference between try?, try!, and try
+
+// Normal try catch block
+do {
+  let myCourse = try Course(name: "")
+} catch NameError.noName {
+  print("Make sure the course has a name")
+}
+
+// If error is not thrown, return an optional
+// If error is thrown, return nil
+let newCourse = try? Course(name: "Swift")
+
+// Force unwrapping
+let anotherCourse = try! Course(name: "Swift")
